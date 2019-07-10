8247232196da6e5da6be9f37d3147f2dfd69e720
Juri Boiko <jboiko@williamhill.co.uk>
2019-07-10 16:25:17 +0200

    redux less code

--- src/components/GoogleAuth.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { connect } from 'react-redux';
-
-
-class GoogleAuth extends React.Component {
-      console.log("GOOGLE AUTH REMOVED")
-}
-
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,23 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-import authReducer from './authReducer';
-import streamReducer from './streamReducer';
+
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Maca2rena', duration: '2:30' },
+    { title: 'All Star', duration: '3:15' },
+    { title: 'I Want it That Way', duration: '1:45' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
 
 export default combineReducers({
-  auth: authReducer,
-  form: formReducer,
-  streams: streamReducer
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
 });
