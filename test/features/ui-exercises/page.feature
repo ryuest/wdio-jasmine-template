68a7f9dc81fbc1b114d71ef91ca4b323956a7f39
Juri Boiko <jboiko@williamhill.co.uk>
2019-08-09 15:00:03 +0200

    good cucu test

--- test/features/ui-exercises/page.feature
+++ test/features/ui-exercises/page.feature
@@ -3,4 +3,4 @@ Feature: Home page
 Scenario: User sees the home page
     Given the Localhost page loaded
     When I go to the top games configuration page
-    Then I drag and Drop the box
+    Then I click on 1 song with name "Maca2rena"
--- test/steps/then.js
+++ test/steps/then.js
@@ -4,19 +4,13 @@ import chai from 'chai';
 import Page from '../support/action/page';
 
 let expect = chai.expect;
-let page = new Page();
 
 
-Then(/^I drag and Drop the box$/, () => {
-    const links = $$('.ui.primary.button')[1];
-    const submit = $('button[type="submit"]');
-     console.log('HERE 777' )
-     submit.click();
-     console.log('HERE 888')
-  //   browser.click(links);
-    console.log('HERE 999' + submit.getText())
-     links.click();
-    console.log('HERE 000' + links.getText())
-    let title = $('[class*="eight wide"].column');
-    expect(title).getText().to.equal('Maca2rena');
+Then(/^I click on (-?\d+) song with name "([^"]*)"$/, (number, keyword) => {
+    const links = $$('.ui.primary.button')[number];
+    links.click();
+    let songMenuTitle = $('.eight').$$('.item')[number].$('div[class="content"]')
+    expect(songMenuTitle.getText()).to.equal(keyword);
+    let selectedSongTitle = $$('.column.eight.wide')[number].$('p')
+    expect(selectedSongTitle.getText()).to.include(keyword)
 });
